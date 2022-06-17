7df2d6ad5cb567bd019235718fb5d400e94a5900
muhyasin89 <muhyasin89@gmail.com>
2022-06-17 08:38:32 +0700

    [master]add checkpoint react-tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{jsx,ts,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
